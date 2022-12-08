831d1fd9e87dbeffef0e751a89b65b8243b8ac85
Henry Sullivan <sullivanbhenry@gmail.com>
2022-12-08 11:39:27 -0800

    Add user interface to project
    Co-Authored-By: Robert Onstott <roonstott@gmail.com>

---
+++ ScrabbleScorer/Program.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using ScrabbleScorer.Models;
+
+namespace Interface
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      Console.WriteLine("Welcome to the Scrabble Scorer!");
+      Console.WriteLine("Please input a word: ");
+      string userInput = Console.ReadLine().ToLower();
+      Scrabble userWord = new Scrabble(userInput);
+
+      Console.WriteLine(userWord.WordScore());
+
+      Console.WriteLine("Would you like to play again? [Y]/[N]");
+      string replay = Console.ReadLine().ToLower();
+
+      if (replay == "y")
+      {
+        Main();
+      } else {
+        Console.WriteLine("Thanks for playing!");
+      }
+    }
+  }
+}
